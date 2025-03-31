82e484eef0add8962a8b0e1a3f6b05e8cf9a9168
ishehara <mlisliyanage@gmail.com>
2025-03-31 10:23:04 +0530

    initail commit

---
+++ src/main/java/com/skillzora/skillzora_backend/SkillzoraBackendApplication.java
@@ -0,0 +1,22 @@
+package com.skillzora.skillzora_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SkillzoraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillzoraBackendApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String rootEndString(){
+		String message = "Hello world!";
+		return message;
+	}
+
+}

a22acbe93ca9591f1e252c8de7ee15220794c6af
ishehara <mlisliyanage@gmail.com>
2025-04-25 16:42:34 +0530

    adding dependencies

--- src/main/java/com/skillzora/skillzora_backend/SkillzoraBackendApplication.java
+++ src/main/java/com/skillzora/skillzora_backend/SkillzoraBackendApplication.java
@@ -2,11 +2,13 @@ package com.skillzora.skillzora_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableMongoRepositories
 public class SkillzoraBackendApplication {
 
 	public static void main(String[] args) {
